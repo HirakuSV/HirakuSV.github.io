1a86eeb4765e666aa3fedbfb8cacfd56b257ad76
HirakuSV <pbawane899@gmail.com>
2022-10-31 14:13:46 +0530

    Imported all JS to a diff file

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const btn = document.querySelector('.button');
+btn.addEventListener('click', function (r) {
+    let x = r.clientX - r.target.offsetLeft;
+    let y = r.clientY - r.target.offsetTop;
+    let ripples = document.createElement('span');
+    ripples.setAttribute('id', 'ripps');
+    ripples.style.left = `${x}px`;
+    ripples.style.top = `${y}px`;
+    this.appendChild(ripples);
+    setTimeout(() => {
+        ripples.remove();
+    }, 1000)
+});
+
 let str = window.location.search.slice(1, 5);
 
 function getParams(paramNames) {
@@ -14,4 +28,8 @@ function setUnData() {
     abtLogo.innerHTML = 'Ray-chan';
 }
 
-if (getParams(str) == 'unofc') setUnData()
+if (getParams(str) == 'unofc') setUnData();
+
+function detectMob() {
+    if (window.innerWidth <= 800 && window.innerHeight <= 600) window.location.href = './notsupported.html'
+}
